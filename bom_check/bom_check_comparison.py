7a6ecce5e7cfac67d415b40f0669bfb6b8d1cb5c
Jack YC Lin <jackyc.lin@adlinktech.com>
2023-08-09 15:45:21 +0800

    add Qty check & update must_have qty in db

--- bom_check/bom_check_comparison.py
+++ bom_check/bom_check_comparison.py
@@ -5,11 +5,15 @@ from connect import Cursor
 import json
 
 # 類似運算元overloading的東西
-class status:
+class bom_status:
     def __init__(self, state: str) -> None:
         self.state = state
     def __passable__(self) -> bool:
-        if self.state == "pass" or self.state == "uncheck":
+        if self.state['result'] == "pass" or self.state == "uncheck":
+            return True
+        return False
+    def check_qty(self, qty: int) -> bool:
+        if int(self.state['Qty']) == qty:
             return True
         return False
 
@@ -21,7 +25,11 @@ def check_erp_in_bom(bom: list, all_name_check_result: dict, extra_problem: str)
             for bom_item in bom:
                 if not item_used and bom_item['itemNumber'] == item['item_no']:
                     item_used = True
-                    bom_item['result'] = "pass" if status(bom_item['result']).__passable__ else bom_item['result']
+                    if not bom_status(bom_item).check_qty(1):
+                        bom_item['result'] = "fail"
+                        extra_problem += f"料件: {item['item_no']}數量有錯\n"
+                    else:
+                        bom_item['result'] = "pass" if bom_status(bom_item).__passable__ else bom_item['result']
             if not item_used:
                 extra_problem += f"ERP名稱帶到的料號: {item['item_no']}沒帶到\n"
     return bom, extra_problem
@@ -35,11 +43,15 @@ def check_must_have(cursor: Cursor, bom: list, factory: str, product_type: str, 
     for part in json.loads(cursor.fetchone()['parts']):
         part_used = False
         for bom_item in bom:
-            if not part_used and bom_item['itemNumber'] == part:
+            if not part_used and bom_item['itemNumber'] == part[0]:
                 part_used = True
-                bom_item['result'] = "pass" if status(bom_item['result']).__passable__ else bom_item['result']
+                if not bom_status(bom_item).check_qty(int(part[1])):
+                    bom_item['result'] = "fail"
+                    extra_problem += f"料件: {part[0]}數量有錯\n"
+                else:
+                    bom_item['result'] = "pass" if bom_status(bom_item).__passable__ else bom_item['result']
         if not part_used:
-            extra_problem += f"必帶料: {part}沒帶到\n"
+            extra_problem += f"必帶料: {part[0]}沒帶到\n"
     return bom, extra_problem
 
 def check_cable(cursor: Cursor, bom: list, is_MXM: bool, extra_problm: str) -> tuple[list, str]:
@@ -55,7 +67,11 @@ def check_cable(cursor: Cursor, bom: list, is_MXM: bool, extra_problm: str) -> t
             for bom_item in bom:
                 if not cable_used and bom_item['itemNumber'] == cable:
                     cable_used = True
-                    bom_item['result'] = "pass" if status(bom_item['result']).__passable__ else bom_item['result']
+                    if not bom_status(bom_item).check_qty(1):
+                        bom_item['result'] = "fail"
+                        extra_problem += f"料件: {cable}數量有錯\n"
+                    else:
+                        bom_item['result'] = "pass" if bom_status(bom_item).__passable__ else bom_item['result']
             if not cable_used:
                 extra_problm += f"cable: {cable}沒帶到\n"
     else:
@@ -82,12 +98,14 @@ def check_FPC(cursor: Cursor, bom: list, is_MXM: bool, description: str, extra_p
             for bom_item in bom:
                 if not fpc_used and bom_item['itemNumber'] == fpc:
                     fpc_used = True
-                    bom_item['result'] = "pass" if status(bom_item['result']).__passable__ else bom_item['result']
+                    if not bom_status(bom_item).check_qty(1):
+                        bom_item['result'] = "fail"
+                        extra_problem += f"料件: {fpc}數量有錯\n"
+                    else:
+                        bom_item['result'] = "pass" if bom_status(bom_item).__passable__ else bom_item['result']
             if not fpc_used:
                 extra_problm += f"fpc: {fpc}沒帶到\n"
     return bom, extra_problm
-
-    
 
 def check_bp_cooler() -> tuple[list, str]:
     '''檢查背板散熱'''
